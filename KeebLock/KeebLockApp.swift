90d890b10e91369bb11aee9d8eb0cdee91fccc81
bmmmm <hi@brtsz.de>
2026-05-04 22:52:34 +0200

    Initial Commit

---
+++ KeebLock/KeebLockApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeebLockApp.swift
+//  KeebLock
+//
+//  Created by Bartosz Makosch on 04.05.26.
+//
+
+import SwiftUI
+
+@main
+struct KeebLockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
